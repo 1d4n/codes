d97d86a36fd243ed4f5fbadcdd673af49b67c174
Idan <55255630+1d4n@users.noreply.github.com>
2020-09-17 11:48:50 +0300

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,30 @@
+__author__ = '__1d4n__'
+
+
+def is_prime(n):
+    """ Checks if an integer is a prime number
+    Args:
+        n = an integer
+    """
+    factors = 0
+    test = 2
+    while test * test <= n:
+        if n % test == 0:
+            factors += 1
+        test += 1
+    return factors == 0  # return True only if there is no factor for the number.
+
+
+if __name__ == "__main__":
+
+    while True:
+        number = input("Please enter a number to check whether it's a prime number (enter -1 to stop): \n")
+        if number.isdigit():
+            if is_prime(int(number)):
+                print("The number", number, "is prime.\n")
+            else:
+                print("The number", number, "is not prime.\n")
+        else:
+            if int(number) == -1:
+                break
+            print("Error: You can only enter a number!\n")
