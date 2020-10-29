fe368f524fffc29d46889b71d420f78be119d3a0
Idan <55255630+1d4n@users.noreply.github.com>
2020-10-29 17:38:13 +0200

    Fibonacci Numbers Generator

--- fibonacci.py
+++ fibonacci.py
@@ -1,48 +1,19 @@
 __author__ = '1d4n'
 
 
-def fib_0(n):
-    """This functions returns a list which contains the first n numbers in the fibonacci sequence, starting with 0.
-    Args:
-        n = a positive integer.
-    """
-    if n.isdigit():
-        if int(n) < 1:
-            return "Error: the number of numbers must be at least 1"
-        elif int(n) == 1:
-            fib_list = [0]
-        else:
-            fib_list = [0, 1]
-        while len(fib_list) < int(n):
-            fib_list.append(fib_list[-1] + fib_list[-2])
-        return fib_list
-
-
-def fib_1(n):
-    """This functions returns a list which contains the first n numbers in the fibonacci sequence, starting with 1.
-    Args:
-        n = a positive integer.
-    """
-    if n.isdigit():
-        if int(n) < 1:
-            return "Error: the number of numbers must be at least 1"
-        elif int(n) == 1:
-            fib_list = [1]
-        else:
-            fib_list = [1, 1]
-        while len(fib_list) < int(n):
-            fib_list.append(fib_list[-1] + fib_list[-2])
-        return fib_list
-
-
-if __name__ == '__main__':  # Checking if the code is running from the current file (and not being imported as a module)
-
+def fibonacci():
+    a, b = 0, 1  # First 2 numbers in the fibonacci sequence
     while True:
-        number = input("How many numbers of the fibonacci sequence to you want to see? (enter 0 to stop)\n")
-        if number == '0':
+        a, b = b, a + b
+        yield a
+
+
+# Checking if the code is running from the current file (and not being imported from another file):
+if __name__ == '__main__':
+    while True:
+        num = input("\nHow many numbers of the fibonacci sequence to you want to see? (0 to stop)\n")
+        if num == '0' or not num.isdigit():
             break
-        if fib_0(number) or fib_1(number) is not None:
-            print("starting with zero:", fib_0(number))
-            print("starting with one:", fib_1(number), "\n")
-        else:
-            print("Error: You must enter a positive number!\n")
+        fib_generator = fibonacci()
+        for i in range(int(num)):
+            print(next(fib_generator), end=" ")
