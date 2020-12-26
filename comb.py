e8488d68e6f42b50bcbb02595fe0e715bba9b4fc
Idan <55255630+1d4n@users.noreply.github.com>
2020-12-26 12:41:12 +0200

    Combinatorics

--- comb.py
+++ comb.py
@@ -9,29 +9,29 @@ def choose(n, k):
     return 0
 
 def solutions(n,m):
-	"""
-	:param n: the number of elements
-	:param m: a natural number
-	return: the number of natural solutions for: x_1 + x_2 +... x_n = m
-	"""
-	a = n -1 + m
-	return choose(a, m)
+    """
+    :param n: the number of elements
+    :param m: a natural number
+    :return: the number of natural solutions for: x_1 + x_2 +...+ x_n = m
+    """
+    if n < 1 or m < 0:
+        return "number of elements must be at least 1 and equals to a non-negative value."
+    a = n -1 + m
+    return choose(a, m)
 
 if __name__ == "__main__":
 
-    x = y = 1
     while True:
-        if x >= 1 and y >= 0:
-            print("x_1 + x_2 + x_3 + ... x_n = m")
-            x = int(input("How many elements? (n = ?) "))
-            y = int(input("What is the value of m? "))
-            print("The number of natural solutions for the equation is:", solutions(x, y))
+        print("x_1 + x_2 + x_3 + ... + x_n = m")
+        x = int(input("How many elements? (n = ?) "))
+        y = int(input("What is the value of m? "))
+        print("The number of natural solutions for the equation is:", solutions(x, y))
 
-            z = 0
-            for i in range(y+1):
-                z += solutions(x-1, i)
-            print(" The number of natural solution for: x_1 + x_2 + x_3 + ... x_n-1 <= m is:", z)
+        z = 0
+        for i in range(y+1):
+            z += solutions(x-1, i)
+        print(" The number of natural solution for: x_1 + x_2 + x_3 + ... + x_n-1 <= m is:", z)
 
-            again = int(input("Again? (0 = No \ 1 Yes) "))
-            if not again:
-                break
+        again = int(input("Again? (0 = No \ 1 Yes) "))
+        if not again:
+            break
