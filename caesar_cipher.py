7078dd894400ee3ed64d8846d7b5b809f526caca
Idan <55255630+1d4n@users.noreply.github.com>
2021-01-07 00:37:15 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -7,20 +7,20 @@ def caesar_enc(msg, shift=1):
     :param shift: A number of alphabet shifts as an integer (default = 1).
     :return: Encrypted string.
     """
-    if type(msg) == str:
-        enc = ''
-        for char in msg:
-            if char.islower():
-                char_idx = string.ascii_lowercase.find(char)
-                enc += string.ascii_lowercase[(char_idx + shift) % 26]
-            elif char.isupper():
-                char_idx = string.ascii_uppercase.find(char)
-                enc += string.ascii_uppercase[(char_idx + shift) % 26]
-            else:
-                enc += char
-        return enc
-    else:
+    if not isinstance(msg, str):
         raise Exception("Can't encrypt a non-string message")
+    enc = ''
+    for char in msg:
+        if char.islower():
+            char_idx = string.ascii_lowercase.find(char)
+            enc += string.ascii_lowercase[(char_idx + shift) % 26]
+        elif char.isupper():
+            char_idx = string.ascii_uppercase.find(char)
+            enc += string.ascii_uppercase[(char_idx + shift) % 26]
+        else:
+            enc += char
+    return enc
+
 
 
 def caesar_dec(msg, shift=1):
@@ -29,24 +29,23 @@ def caesar_dec(msg, shift=1):
     :param shift: A number of alphabet shifts (as an integer ,default = 1).
     :return: Decrypted string.
     """
-    if type(msg) == str:
-        dec = ''
-        for char in msg:
-            if char.islower():
-                char_idx = string.ascii_lowercase.find(char)
-                dec += string.ascii_lowercase[(char_idx - shift) % 26]
-            elif char.isupper():
-                char_idx = string.ascii_uppercase.find(char)
-                dec += string.ascii_uppercase[(char_idx - shift) % 26]
-            else:
-                dec += char
-        return dec
-    else:
-        raise Exception("Can't decrypt a non-string message")
+    if not isinstance(msg, str):
+        raise Exception("Can't encrypt a non-string message")
+    dec = ''
+    for char in msg:
+        if char.islower():
+            char_idx = string.ascii_lowercase.find(char)
+            dec += string.ascii_lowercase[(char_idx - shift) % 26]
+        elif char.isupper():
+            char_idx = string.ascii_uppercase.find(char)
+            dec += string.ascii_uppercase[(char_idx - shift) % 26]
+        else:
+            dec += char
+    return dec
 
 
 if __name__ == '__main__':
-    txt = "Hello World!"
+    txt = input("Enter the text that you want to ecrypt / decrypt: ")
     encrypted = caesar_enc(txt, 3)
     decrypted = caesar_dec(encrypted, 3)
     print('Encrypted text:', encrypted)
